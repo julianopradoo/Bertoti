60aaa33095aa7f476e80582124144b2aed8cb312
Juliano Prado <125655127+julianopradoo@users.noreply.github.com>
2023-05-18 10:29:48 -0300

    Update NewMain.java

--- Bertoti/Engenharia de Software/Diagrama_de_classe/src/NewMain.java
+++ Bertoti/Engenharia de Software/Diagrama_de_classe/src/NewMain.java
@@ -15,7 +15,9 @@ import modelos.PessoaJuridica;
  */
 public class NewMain {
 
-    /**
+    
+
+/**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
